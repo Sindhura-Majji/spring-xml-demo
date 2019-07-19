3ef3c64bda98259a306beb71ba3302afac347b68
Sindhura-Majji <51950216+Sindhura-Majji@users.noreply.github.com>
2019-07-19 12:25:16 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -14,10 +14,10 @@ public class Main {
         ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
         System.out.println("display using ApplicationContext");
 
-
+        //getting movie bean of id movie
         Movie movie1 = context.getBean("movie" , Movie.class);
         movie1.movie();
-
+        //getting movie bean of id movie1
         Movie movie2 = context.getBean("movie1" , Movie.class);
         movie2.movie();
 
